69bef2cb5f34c27f29848a2ca030b728f8ad3569
Jared Gillespie <jaredlgillespie@hotmail.com>
2018-03-29 10:47:23 -0400

    Updating authors

--- iobs.py
+++ iobs.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = 'Jared Gillespie'
+__author__ = 'Jared Gillespie, Martin Heil'
 __version__ = '0.2.0'
 
 
